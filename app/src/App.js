bcadb5c0b3a77e23866285270400a1ec2464e741
Willians Nieves <williamsnieves@gmail.com>
2019-06-30 22:30:08 +0200

    master - NF - added component App as container of the widget

--- app/src/App.js
+++ app/src/App.js
@@ -1,1 +1,58 @@
-App.js
+import React from 'react'
+import FinanceWidget from './FinanceWidget'
+import CreditAgreements from './modules/CreditAgreements'
+import { getPurchasePriceFormated } from './modules/utils'
+import PaymentInfo from './common/PaymentInfo'
+
+class App extends React.Component {
+  state = {
+    dataCreditAfreement: null,
+    instalmentFee: ''
+  }
+  constructor (props) {
+    super(props)
+    this.creditAgreemensts = new CreditAgreements()
+  }
+
+  onToggleModal = () => {
+    this.setState({
+      showModal: !this.state.showModal
+    })
+  }
+
+  updatePrice (price) {
+    if (price !== undefined) {
+      const formatedPrice = getPurchasePriceFormated(price)
+      this.creditAgreemensts
+        .fetchCreditAgreements(formatedPrice)
+        .then(dataCreditAfreement =>
+          this.setState({
+            dataCreditAfreement
+          })
+        )
+    }
+  }
+
+
+  onHandleAgreements = select => {
+    const { instalmentFee } = JSON.parse(select.target.value)
+    this.setState({
+      instalmentFee
+    })
+  }
+
+  render () {
+    const { dataCreditAfreement, showModal, instalmentFee } = this.state
+    return (
+      <React.Fragment>
+        <FinanceWidget
+          dataCreditAfreement={dataCreditAfreement}
+          onToggleModal={this.onToggleModal}
+          onHandleAgreements={this.onHandleAgreements}
+        />
+      </React.Fragment>
+    )
+  }
+}
+
+export default App
